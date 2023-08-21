330cb360c1fd39a473fd3196291d673ac85408a2
Náthan Luciano <118984512+Tylerzeraa@users.noreply.github.com>
2023-08-20 21:27:39 -0300

    Commit inicial

---
+++ ExemploAPI/AlunovViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ExemploAPI.Models
+{
+    public class AlunoViewModel
+    {
+        public string Nome { get; set; }
+        public string Ra { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        
+        public int Id { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
---
+++ ExemploAPI/Controllers/AlunoController.cs
@@ -0,0 +1,75 @@
+﻿using ExemploAPI.Models;
+using ExemploAPI.Models.Response;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Linq;
+using ExemploAPI;
+
+namespace ExemploAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunoController : ControllerBase
+    {
+        private static List<AlunoViewModel> _alunos = new List<AlunoViewModel>();
+        private static int _nextId = 1;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_alunos);
+        }
+    }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var aluno = _alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null)
+                return NotFound();
+
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post(AlunoViewModel alunoViewModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            alunoViewModel.Id = _nextId++;
+            _alunos.Add(alunoViewModel);
+
+            return Ok(new NovoAlunoCriadoResponse { Sucesso = true, Mensagem = "Aluno criado com sucesso!" });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, AlunoViewModel alunoViewModel)
+        {
+            var alunoExistente = _alunos.FirstOrDefault(a => a.Id == id);
+            if (alunoExistente == null)
+                return NotFound();
+
+            alunoViewModel.Id = id;
+            _alunos.Remove(alunoExistente);
+            _alunos.Add(alunoViewModel);
+
+            return Ok(new NovoAlunoCriadoResponse { Sucesso = true, Mensagem = "Aluno atualizado com sucesso!" });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var alunoExistente = _alunos.FirstOrDefault(a => a.Id == id);
+            if (alunoExistente == null)
+                return NotFound();
+
+            _alunos.Remove(alunoExistente);
+
+            return Ok(new NovoAlunoCriadoResponse { Sucesso = true, Mensagem = "Aluno removido com sucesso!" });
+        }
+    }
+}
