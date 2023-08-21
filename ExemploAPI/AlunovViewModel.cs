c7a104dd6374534c2b8fee1e1e672a040cfba766
ryano <ryano@DESKTOP-S9PH8SD>
2023-08-20 21:34:56 -0300

    Commit teste

--- ExemploAPI/AlunovViewModel.cs
+++ ExemploAPI/AlunovViewModel.cs
@@ -6,8 +6,10 @@
         public string Ra { get; set; }
         public string Email { get; set; }
         public string Cpf { get; set; }
-        
+
         public int Id { get; set; }
         public bool Ativo { get; set; }
+
+        // teste
     }
 }
